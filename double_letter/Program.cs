dadde25442768ea3bb42f18bcb6ed73974e5945a
shvagr0 <vladislav.rozvadovski@kpk-lp.com.ua>
2021-11-03 18:49:19 +0200

    add my work

---
+++ double_letter/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavdanya_from_ostap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string[] words = text.Split(' ');
+
+            Console.WriteLine();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length - 1; j++)
+                {
+
+                    if (words[i][j] == words[i][j + 1])
+                    {
+                        Console.Write(words[i] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
